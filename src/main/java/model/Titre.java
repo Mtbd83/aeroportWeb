f4b3d48797126c43a5492383973af6459c168ef4
AuroreD <44267763+AuroreD@users.noreply.github.com>
2018-11-21 14:20:41 +0100

    Update Titre.java

--- src/main/java/model/Titre.java
+++ src/main/java/model/Titre.java
@@ -4,8 +4,17 @@ public enum Titre {
 	M("monsieur"), MME("madame"), MLLE("mademoiseille");
 
 	private String titre;
-	
+
 	private Titre(String titre) {
-		
+		this.titre = titre;
 	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	
 }
