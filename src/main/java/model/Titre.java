1fe80cbf9e8172988afd42b4decdefea5d344960
Mtbd83 <m.tribaudeau@gmail.com>
2018-11-21 10:04:10 +0100

    Initial commit

---
+++ src/main/java/model/Titre.java
@@ -0,0 +1,11 @@
+package model;
+
+public enum Titre {
+	M("monsieur"), MME("madame"), MLLE("mademoiseille");
+
+	private String titre;
+	
+	private Titre(String titre) {
+		
+	}
+}
---
+++ src/main/java/repositories/PassagerRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Passager;
+
+public interface PassagerRepository extends JpaRepository<Passager, Integer>{
+
+}
